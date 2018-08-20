3008b729760edb7efe4badf9428e34d19718dc7a
rafaelfccg <rafaelfccg@gmail.com>
2018-08-20 19:26:29 -0300

    make timeInForce optional

--- service_account.go
+++ service_account.go
@@ -30,7 +30,12 @@ func (as *apiService) NewOrder(or NewOrderRequest) (*ProcessedOrder, error) {
 	params["symbol"] = or.Symbol
 	params["side"] = string(or.Side)
 	params["type"] = string(or.Type)
-	params["timeInForce"] = string(or.TimeInForce)
+
+	timeInForce := string(or.TimeInForce)
+	if timeInForce != "" {
+		params["timeInForce"] = string(or.TimeInForce)
+	}
+
 	params["quantity"] = strconv.FormatFloat(or.Quantity, 'f', -1, 64)
 	params["price"] = strconv.FormatFloat(or.Price, 'f', -1, 64)
 	params["timestamp"] = strconv.FormatInt(unixMillis(or.Timestamp), 10)
